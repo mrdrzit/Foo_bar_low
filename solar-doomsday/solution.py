1a730193ca36b637c870469c597550ebc35bf297
mrdrzit <uzunaruto.10.mat@gmail.com>
2022-04-30 17:36:00 -0300

    Info and first challenge

---
+++ solar-doomsday/solution.py
@@ -0,0 +1,6 @@
+def solution(area):
+  '''solution(area) takes as its input a single unit of measure representing the total area of solar panels you have 
+  (between 1 and 1000000 inclusive) and returns a list of the areas of the largest squares you could make out of those panels, 
+  starting with the largest squares first. So, solution(12) would return [9, 1, 1, 1].'''
+  # Your code here
+  pass
